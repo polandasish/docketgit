123eee8297462f92c188018b2cfaab2d407cb4e9
polandasish <139715583+polandasish@users.noreply.github.com>
2024-05-19 12:52:22 +0200

    Create args.sh

---
+++ args.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Script name: $0"
+echo "enter username: $1"
+echo "enter 2nd username: $2"
+echo "Total no. of arguments: $#"
+echo "Arguments as list: $@"
+echo "Arguments as string: $*"
